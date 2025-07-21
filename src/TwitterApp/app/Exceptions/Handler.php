431a896067d6d47a03195f1067688fd9dd427679
“hiroshi-git147” <hiroshigit@gmail.com>
2025-07-21 22:30:41 +0900

    認証時401レスポンスを受けとる

--- src/TwitterApp/app/Exceptions/Handler.php
+++ src/TwitterApp/app/Exceptions/Handler.php
@@ -4,6 +4,8 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Http\Request;
+use Illuminate\Auth\AuthenticationException;
 
 class Handler extends ExceptionHandler
 {
@@ -43,8 +45,12 @@ class Handler extends ExceptionHandler
      */
     public function register()
     {
-        $this->reportable(function (Throwable $e) {
-            //
+        $this->renderable(function (AuthenticationException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => '認証の有効期限が切れました。再度ログインしてください。'
+                ], 401);
+            }
         });
     }
 }
