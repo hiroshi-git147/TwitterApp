087c1c29bf7597afef90a4b0101b4ae0e023b0ec
“hiroshi-git147” <hiroshigit@gmail.com>
2025-08-02 22:46:39 +0900

    TweetController PHPDOC追加

--- src/TwitterApp/app/Http/Controllers/TweetController.php
+++ src/TwitterApp/app/Http/Controllers/TweetController.php
@@ -7,23 +7,45 @@ use Illuminate\View\View;
 
 class TweetController extends Controller {
 
+    /**
+     * 投稿一覧を表示する
+     * 
+     * @return View
+     */
     public function index(): View
     {
         return view('tweets.index');
     }
 
+    /**
+     * 投稿詳細を表示する
+     * 
+     * @param Tweet $tweet 投稿
+     * @return View
+     */
     public function show(Tweet $tweet): View
     {
         $this->authorize('view', $tweet);
         return view('tweets.show', compact('tweet'));
     }
 
+    /**
+     * 投稿を作成する
+     * 
+     * @return View
+     */
     public function create(): View
     {
         $this->authorize('create', Tweet::class);
         return view('tweets.create');
     }
 
+    /**
+     * 投稿を更新する
+     * 
+     * @param Tweet $tweet 投稿
+     * @return View
+     */
     public function edit(Tweet $tweet): View
     {
         $this->authorize('update', $tweet);
